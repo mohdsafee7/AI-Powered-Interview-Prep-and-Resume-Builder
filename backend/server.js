db616bfecad5492c27672735a2c5520e6f4e13de
Mohd Safee <2k23.cs2313689@gmail.com>
2026-08-16 01:06:26 +0530

    Integrated Gemini-3.6-flash

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectToDB = require('./src/config/database.js');
-
+const invokeGeminiAi = require('./src/services/ai.service.js');
 const startServer = async () => {
 
   await connectToDB();
+
+  await invokeGeminiAi();
 
   app.listen(3000, () => {
     console.log("Server is running on port 3000");
